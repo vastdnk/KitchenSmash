11c9ae4764ca434e940c57ad8ed4efaf50d24cc2
Vladimir Stadnik <va.stdnk@gmail.com>
2024-01-09 02:02:25 +0300

    _

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,32 +9,27 @@ public class Player : MonoBehaviour
 
     private bool isWalking;
 
+    [SerializeField] private GameInput gameInput;
+
     private void Update()
     {
 
-        Vector2 inputVector = new Vector2(0,0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+
+
+
+        float playerSize = .7f;
+        bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize);
+
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
+
+        
 
         isWalking = moveDir != Vector3.zero;
 
