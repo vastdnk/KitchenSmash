b80451267947cb84543352c29669a67641b591ee
Vladimir Stadnik <va.stdnk@gmail.com>
2024-03-13 23:42:31 +0300

    Some changes

---
+++ Assets/Scripts/ContainerCounter.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContainerCounter : MonoBehaviour, IKitchenObjectParent
+{
+
+
+
+    [SerializeField] private KitchenObjectsSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
+    private KitchenObject kitchenObject;
+
+    public void Interact(Player player)
+    {
+
+
+        if (kitchenObject == null)
+        {
+
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
+            kitchenObjectTransform.localPosition = Vector3.zero;
+
+            kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+            kitchenObject.SetKitchenObjectParent(this);
+        }
+        else
+        {
+            //GIVE
+            kitchenObject.SetKitchenObjectParent(player);
+        }
+
+    }
+
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
+}
